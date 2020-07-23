1fb937a70549bfc999511f963ba06404819e8763
StalkyTalky <mahmoudahmed20102223@gmail.com>
2020-07-23 07:55:55 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='fullWindowEXPLORER'>
+      <span class="topBar">
+          <p id="title">World View Menu</p>
+          <button id="leaveApp" class="Buttons">X</button>
+          <button id="fullScreen" class="Buttons">□</button>
+          <button id="minimizeApp" class="Buttons">_</button>
+      </span>
     </div>
   );
 }
